6a709a2e36e04c56079e1e7df7cd90b968910090
jyguo <1066146355@qq.com>
2017-12-26 15:12:17 +0800

    dubbo入门案例

--- dubbo-provider/src/main/java/com/test/DemoServiceImpl.java
+++ dubbo-provider/src/main/java/com/test/DemoServiceImpl.java
@@ -8,7 +8,7 @@ public class DemoServiceImpl implements DemoService{
 	@Override
 	public String sayHello(String name) {
 		// TODO Auto-generated method stub
-		return name; 
+		return name;
 	}  
 
 }
